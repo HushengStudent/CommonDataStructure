296692a33bd5b7c63999b8449dcb7a6462295782
HushengStudent <husheng.student@foxmail.com>
2017-07-24 00:14:10 +0800

    create project

---
+++ CommonDataStructure/Program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void main()
+{
+	cout << "test";
+	system("pause");
+	return;
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CommonDataStructure
 Using C++ to realize common data structure.
+
+software：visual studio2013.
+
+progress：
